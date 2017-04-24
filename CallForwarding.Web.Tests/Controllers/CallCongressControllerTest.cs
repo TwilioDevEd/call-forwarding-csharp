ff2ef7fada68fb7e6e2a9e22907271be45a417ad
Hector Ortega <hector.oj@gmail.com>
2017-04-24 03:30:48 -0500

    Add CallCongress controller

---
+++ CallForwarding.Web.Tests/Controllers/CallCongressControllerTest.cs
@@ -0,0 +1,182 @@
+﻿using CallForwarding.Web.Controllers;
+using CallForwarding.Web.Models;
+using CallForwarding.Web.Models.Repository;
+using CallForwarding.Web.Tests.Extensions;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Xml.XPath;
+using TestStack.FluentMVCTesting;
+
+namespace CallForwarding.Web.Tests.Controllers
+{
+    class CallCongressControllerTest
+    {
+        private CallCongressController _controller;
+
+        [SetUp]
+        public void setUp ()
+        {
+            _controller = new CallCongressController();
+        }
+
+        [Test]
+        public void WelcomeRouteTriggersLookupStateWhenNoFromStateParameter()
+        {
+            _controller.WithCallTo(c => c.Welcome(null))
+                .ShouldReturnTwiMLResult(data =>
+                {
+                    StringAssert.Contains("please enter your 5 - digit zip code", 
+                        data.XPathSelectElement("Response/Say").Value);
+                    StringAssert.AreEqualIgnoringCase("/callcongress/statelookup",
+                        data.XPathSelectElement("Response/Gather").Attribute("action").Value);
+                    StringAssert.AreEqualIgnoringCase("5",
+                        data.XPathSelectElement("Response/Gather").Attribute("numDigits").Value);
+                });
+        }
+
+        [Test]
+        public void WelcomeRouteTriggerSetStateWhenFromStateParameterIsPresent()
+        {
+            _controller.WithCallTo(c => c.Welcome("PR"))
+                .ShouldReturnTwiMLResult(data =>
+                {
+                    StringAssert.Contains("If this is correct, please press 1. Press 2 if",
+                        data.XPathSelectElement("Response/Say").Value);
+                    StringAssert.AreEqualIgnoringCase("/callcongress/setstate",
+                        data.XPathSelectElement("Response/Gather").Attribute("action").Value);
+                    StringAssert.AreEqualIgnoringCase("1",
+                        data.XPathSelectElement("Response/Gather").Attribute("numDigits").Value);
+                });
+        }
+
+        [Test]
+        public void CollectZipRouteTriggersStateLookup()
+        {
+            _controller.WithCallTo(c => c.CollectZip())
+                .ShouldReturnTwiMLResult(data =>
+                {
+                    StringAssert.Contains("enter your 5-digit zip code.",
+                        data.XPathSelectElement("Response/Say").Value);
+                    StringAssert.AreEqualIgnoringCase("/callcongress/statelookup",
+                        data.XPathSelectElement("Response/Gather").Attribute("action").Value);
+                    StringAssert.AreEqualIgnoringCase("5",
+                        data.XPathSelectElement("Response/Gather").Attribute("numDigits").Value);
+                });
+        }
+
+
+        [Test]
+        public void StateLookupRouteRedirectsToCallSenators()
+        {
+            // given
+            var mockRepository = new Mock<IRepository<Zipcode>>();
+            mockRepository.Setup(r => r.FirstOrDefault(It.IsAny<Func<Zipcode, bool>>()))
+                .Returns(new Zipcode() { State= "PR" });
+            var controller = new CallCongressController(mockRepository.Object, null, null);
+
+            // when
+            var result = (RedirectToRouteResult)controller.StateLookup(12345);
+
+            // then
+            Assert.AreEqual("CallSenators", result.RouteValues["action"]);
+            Assert.AreEqual("PR", result.RouteValues["callerState"]);
+            Assert.IsNull(result.RouteValues["controller"]);
+        }
+
+        [Test]
+        public void testSetStateRouteRedirectToCallSenatorsWhenParametersIs1()
+        {
+            // when
+            var result = (RedirectToRouteResult)_controller.SetState("1", "PR");
+
+            // then
+            Assert.AreEqual("CallSenators", result.RouteValues["action"]);
+            Assert.AreEqual("PR", result.RouteValues["CallerState"]);
+            Assert.IsNull(result.RouteValues["controller"]);
+        }
+
+        [Test]
+        public void SetStateRouteRedirectToCollectZipcodeWhenParametersIs2()
+        {
+            // when
+            var result = (RedirectToRouteResult)_controller.SetState("2", "PR");
+
+            // then
+            Assert.AreEqual("CollectZip", result.RouteValues["action"]);
+            Assert.IsNull(result.RouteValues["controller"]);
+        }
+
+        [Test]
+        public void CallSenatorsRouteTriggersCallToFirstSenator()
+        {
+            // given
+            var senators = new List<Senator>()
+            {
+                new Senator() { Name = "senator1", Phone = "phone1" },
+                new Senator() { Id = 33, Name = "senator2" }
+            };
+            var mockRepository = new Mock<IRepository<State>>();
+            mockRepository.Setup(r => r.FirstOrDefault(It.IsAny<Func<State, bool>>()))
+                .Returns(new State() { Senators = senators });
+            var controller = new CallCongressController(null, null, mockRepository.Object);
+
+
+            // when
+            controller.WithCallTo(c => c.CallSenators(""))
+                .ShouldReturnTwiMLResult(data =>
+                {
+                    // then
+                    StringAssert.Contains("Connecting you to senator1.",
+                        data.XPathSelectElement("Response/Say").Value);
+                    StringAssert.Contains("be re-directed to senator2.",
+                        data.XPathSelectElement("Response/Say").Value);
+                    StringAssert.AreEqualIgnoringCase("/callcongress/callsecondsenator/33",
+                        data.XPathSelectElement("Response/Dial").Attribute("action").Value);
+                    StringAssert.AreEqualIgnoringCase("phone1",
+                        data.XPathSelectElement("Response/Dial").Value);
+                });
+        }
+
+
+        [Test]
+        public void CallSecondSenatorsRouteTriggersCallToSecondSenator()
+        {
+            // given
+            var mockRepository = new Mock<IRepository<Senator>>();
+            mockRepository.Setup(r => r.Find(It.IsAny<int>()))
+                .Returns(new Senator() { Name = "senator", Phone = "phone" });
+            var controller = new CallCongressController(null, mockRepository.Object, null);
+
+            // when
+            controller.WithCallTo(c => c.CallSecondSenator(1))
+                .ShouldReturnTwiMLResult(data =>
+                {
+                    // then
+                    StringAssert.Contains("Connecting you to senator.",
+                        data.XPathSelectElement("Response/Say").Value);
+                    StringAssert.AreEqualIgnoringCase("/callcongress/goodbye",
+                        data.XPathSelectElement("Response/Dial").Attribute("action").Value);
+                    StringAssert.AreEqualIgnoringCase("phone",
+                        data.XPathSelectElement("Response/Dial").Value);
+                });
+        }
+
+        [Test]
+        public void GoodbyeRouteTriggersHangupCall()
+        {
+            // when
+            _controller.WithCallTo(c => c.Goodbye())
+                .ShouldReturnTwiMLResult(data =>
+                {
+                    // then
+                    StringAssert.Contains("Your voice makes a difference. Goodbye.",
+                        data.XPathSelectElement("Response/Say").Value);
+                    Assert.IsNotNull(data.XPathEvaluate("Response/Dial"));
+                });
+        }
+       
+    }
+}
---
+++ CallForwarding.Web/Controllers/CallCongressController.cs
@@ -0,0 +1,145 @@
+﻿using CallForwarding.Web.Models;
+using CallForwarding.Web.Models.Repository;
+using System.Linq;
+using System.Web.Mvc;
+using Twilio.AspNet.Mvc;
+using Twilio.TwiML;
+
+namespace CallForwarding.Web.Controllers
+{
+    public class CallCongressController : TwilioController
+    {
+        private readonly IRepository<Zipcode> _zipcodesRepository;
+        private readonly IRepository<State> _statesRepository;
+        private readonly IRepository<Senator> _senatorsRepository;
+
+        public CallCongressController()
+        {
+            _zipcodesRepository = new ZipcodesRepository();
+            _statesRepository = new StatesRepository();
+            _senatorsRepository = new SenatorsRepository();
+        }
+
+        public CallCongressController(
+            IRepository<Zipcode> zipcodesRepository,
+            IRepository<Senator> senatorsRepository,
+            IRepository<State> statesRepository)
+        {
+            _zipcodesRepository = zipcodesRepository;
+            _statesRepository = statesRepository;
+            _senatorsRepository = senatorsRepository;
+        }
+
+        // Verify or collect State information.
+        [HttpPost]
+        public ActionResult Welcome(string fromState)
+        {
+            var voiceResponse = new VoiceResponse();
+            if (!string.IsNullOrEmpty(fromState))
+            {
+                voiceResponse.Say(string.Format("Thank you for calling congress! It looks like" +
+                                "you\'re calling from {0}." +
+                                "If this is correct, please press 1. Press 2 if" +
+                                "this is not your current state of residence.", fromState));
+                voiceResponse.Gather(numDigits: 1, action: "/callcongress/setstate", method: "POST");
+            }
+            else
+            {
+                voiceResponse.Say("Thank you for calling Call Congress! If you wish to" +
+                                "call your senators, please enter your 5 - digit zip code.");
+                voiceResponse.Gather(numDigits: 5, action: "/callcongress/statelookup", method: "POST");
+            }
+            return TwiML(voiceResponse);
+        }
+
+        // If our state guess is wrong, prompt user for zip code.
+        [HttpPost]
+        public ActionResult CollectZip()
+        {
+            var voiceResponse = new VoiceResponse();
+
+            voiceResponse.Say("If you wish to call your senators, please " +
+                    "enter your 5-digit zip code.");
+            voiceResponse.Gather(numDigits: 5, action: "/callcongress/statelookup", method: "POST");
+
+            return TwiML(voiceResponse);
+        }
+
+        // Look up state from given zipcode.
+        // Once state is found, redirect to call_senators for forwarding.
+        [AcceptVerbs("GET", "POST")]
+        public ActionResult StateLookup(int digits)
+        {
+            // NB: We don't do any error handling for a missing/erroneous zip code
+            // in this sample application. You, gentle reader, should to handle that
+            // edge case before deploying this code.
+            Zipcode zipcodeObject = _zipcodesRepository.FirstOrDefault(z => z.ZipcodeNumber == digits);
+
+            return RedirectToAction("CallSenators", new { callerState = zipcodeObject.State });
+        }
+
+
+        // Set state for senator call list.
+        // Set user's state from confirmation or user-provided Zip.
+        // Redirect to call_senators route.
+        [AcceptVerbs("GET", "POST")]
+        public ActionResult SetState(string digits, string callerState)
+        {
+            if (digits.Equals("1"))
+            {
+                return RedirectToAction("CallSenators", new { callerState = callerState });
+            }
+            else
+            {
+                return RedirectToAction("CollectZip");
+            }
+        }
+
+        // Route for connecting caller to both of their senators.
+        [AcceptVerbs("GET", "POST")]
+        public ActionResult CallSenators(string callerState)
+        {
+            var senators = _statesRepository
+                .FirstOrDefault(s => s.name == callerState)
+                .Senators.ToList();
+
+            var voiceResponse = new VoiceResponse();
+            var firstCall = senators[0];
+            var secondCall = senators[1];
+            var sayMessage = string.Format("Connecting you to {0}. " +
+                "After the senator's office ends the call, you will " +
+                "be re-directed to {1}.", firstCall.Name, secondCall.Name);
+
+            voiceResponse.Say(sayMessage);
+            voiceResponse.Dial(number: firstCall.Phone,
+                action: "/callcongress/callsecondsenator/" + secondCall.Id);
+
+            return TwiML(voiceResponse);
+        }
+
+        // Forward the caller to their second senator.
+        [AcceptVerbs("GET", "POST")]
+        public ActionResult CallSecondSenator(int id)
+        {
+            var senator = _senatorsRepository.Find(id);
+
+            var voiceResponse = new VoiceResponse();
+            voiceResponse.Say(string.Format("Connecting you to {0}.", senator.Name));
+            voiceResponse.Dial(number: senator.Phone, action: "/callcongress/goodbye");
+
+            return TwiML(voiceResponse);
+        }
+
+        // Thank user & hang up.
+        [AcceptVerbs("GET", "POST")]
+        public ActionResult Goodbye()
+        {
+            var voiceResponse = new VoiceResponse();
+            voiceResponse.Say("Thank you for using Call Congress! " +
+                "Your voice makes a difference. Goodbye.");
+            voiceResponse.Hangup();
+
+            return TwiML(voiceResponse);
+        }
+    }
+}
